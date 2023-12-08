806f189fe4f53ddde0177f70cb27fe553b07fe77
Nick <nick.bertocco@gmail.com>
2023-12-08 15:40:58 +0100

    Improved preprocess

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -1,110 +1,121 @@
+import zipfile
 import re
 import numpy as np
 import pandas as pd
 import os
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
-make_path = lambda path: os.path.join(script_directory, path)
+
+
+def unzip_file(zip_file_path, extract_to_path):
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+        zip_ref.extractall(extract_to_path)
+
+
+# Unzip t he ml-latest and ml-latest-small datasets
+# unzip_file(os.path.join(script_directory, "ml-latest-small.zip"), script_directory)
+# unzip_file(os.path.join(script_directory, "ml-latest.zip"), script_directory)
+
+# Helper functions for constructing the input paths
+make_path_full = lambda path: os.path.join(script_directory, "ml-latest", path)
+make_path_small = lambda path: os.path.join(script_directory, "ml-latest-small", path)
+
+# Check if output path exists, create it otherwise
+output_dir = os.path.join(script_directory, "preprocessed")
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+make_output_path = lambda path: os.path.join(output_dir, path)
 
 # Loading full datasets
-ratings_full = pd.read_csv(make_path("ratings_full.csv"))
-tags_full = pd.read_csv(make_path("tags_full.csv"))
-movies_full = pd.read_csv(make_path("movies_full.csv"))
-genome_scores = pd.read_csv(make_path("genome-scores_full.csv"))
-genome_tags = pd.read_csv(make_path("genome-tags_full.csv"))
+ratings_full = pd.read_csv(make_path_full("ratings.csv"))
+genome_scores = pd.read_csv(make_path_full("genome-scores.csv"))
+genome_tags = pd.read_csv(make_path_full("genome-tags.csv"))
 
 # Loading small datasets
-ratings_small = pd.read_csv(make_path("ratings_small.csv"))
-tags_small = pd.read_csv(make_path("tags_small.csv"))
-movies_small = pd.read_csv(make_path("movies_small.csv"))
-
-print(ratings_small["userId"].nunique())
-print(ratings_full["userId"].nunique())
+ratings_small = pd.read_csv(make_path_small("ratings.csv"))
+tags_small = pd.read_csv(make_path_small("tags.csv"))
+movies_small = pd.read_csv(make_path_small("movies.csv"))
 
 
-# Function to remove 'IMAX' from genres column
 def remove_imax(genre_string):
     genres_list = genre_string.split("|")
     genres_list = [genre for genre in genres_list if genre != "IMAX"]
     return "|".join(genres_list)
 
 
-# Apply the function to the 'genres' column
 movies_small["genres"] = movies_small["genres"].apply(remove_imax)
 
-# Filter full tags
 genome_tags["tag"] = genome_tags["tag"].str.lower()
-
-
 for index, row in genome_tags.iterrows():
-    tag = row.tag  # .split()  ## splitting words for spell check
+    tag = row.tag
 
-    ## Removing all parenthesis and its contents, including the whitespace before
+    # Removing all parenthesis and its contents, including the whitespace before
     correct_tag = re.sub(r" \([^)]*\)", "", tag)
 
-    if (
-        "based" in correct_tag
-    ):  ## This is necessary because it is a common tag and avoids the other if statements downstream
+    # This is necessary because it is a common tag and avoids the other if statements downstream
+    if "based" in correct_tag:
         genome_tags.loc[index, "tag"] = correct_tag  # type: ignore
         continue
 
-    if (
-        "-" in correct_tag
-    ):  ## This is to keep "sci-fi" from being removed in the next if statement
+    # This is to keep "sci-fi" from being removed in the next if statement
+    if "-" in correct_tag:
         genome_tags.loc[index, "tag"] = correct_tag  # type: ignore
         continue
 
+    # Replacing two-letter words; Need to maintain index ordering, will delete NaNs later
     if re.findall(r"\b\w{2}\b", correct_tag):
-        ## Replacing two-letter words; Need to maintain index ordering, will delete NaNs later
         genome_tags.loc[index, "tag"] = np.NaN  # type: ignore
 
+    # Replacing one-letter words
     elif re.findall(r"\b\w{1}\b", correct_tag):
-        # Replacing one-letter words
         genome_tags.loc[index, "tag"] = np.NaN  # type: ignore
 
-    elif (
-        tag == correct_tag
-    ):  ## This is for better performance since replacing significantly slows the process
+    # This is for better performance since replacing significantly slows the process
+    elif tag == correct_tag:
         continue
 
+    # Saves the corrected tag
     else:
-        # Saves the corrected tag
         genome_tags.loc[index, "tag"] = correct_tag  # type: ignore
         pass
-
-# Dropping all tags with words that are lower than two letters or less
 genome_tags = genome_tags.dropna()
 
+# Remove tag relevance for movies that are not to be considered
+genome_scores = genome_scores[genome_scores["movieId"].isin(tags_small["movieId"])]
 
+# Merge the tags with their scores obtaining on each row a pair tag, movie and the relevance of the tag
 full_genome = pd.merge(genome_tags, genome_scores, on="tagId", how="inner")
 
-full_genome = full_genome[full_genome["movieId"].isin(tags_small["movieId"])]
+# Inner merge the movies dataframe with the tags dataframe, thus removing movies without tags
+merged_df = pd.merge(movies_small, full_genome, on="movieId", how="inner")
 
-merged_df = pd.merge(movies_small, full_genome, on="movieId")
+# Sort by relevance in descending order
+merged_df.sort_values(by="relevance", ascending=False, inplace=True)
 
-# Sort by 'relevance' in descending order
-merged_df = merged_df.sort_values(by="relevance", ascending=False)
+# Group by movieId and take the top 50 tags for each movie
+merged_df = merged_df.groupby("movieId").head(50)
 
-# Group by 'movieId' and take the top 50 rows for each group
-top_50_tags_per_movie = merged_df.groupby("movieId").head(50)
-tags = top_50_tags_per_movie[["tagId", "movieId", "tag"]].drop_duplicates()
-tags.to_csv(os.path.join(script_directory, "preprocessed", "tags.csv"), index=False)
+# Retrieve surviving tags and save the dataframe to file system
+tags = merged_df[["tagId", "movieId", "tag"]]
+tags.to_csv(make_output_path("tags.csv"), index=False)
 
+# Obtain the unique movie ids of the movies which survived the trimming
 surviving_movie_ids = merged_df["movieId"].unique()
 
+# Filter the movies dataframe with the surviving movie ids and save the dataframe to file system
 movies_filtered = movies_small[movies_small["movieId"].isin(surviving_movie_ids)]
+movies_filtered.to_csv(make_output_path("movies.csv"), index=False)
 
-movies_filtered.to_csv(
-    os.path.join(script_directory, "preprocessed", "movies.csv"), index=False
+# Select all unique user ids from the small ratings dataset and add an arbitrary amount of unique
+# user ids from the full ratings dataset, such that the final result will contain ~100k ratings
+user_ids = np.concatenate(
+    (ratings_small["userId"].unique(), ratings_full["userId"].unique()[-1420:])
 )
 
-user_ids = ratings_small["userId"].unique()
-
-
+# Filter the full ratings dataset by removing movies that were trimmed and users who are not present in the
+# small ratings dataset, then save the dataframe to file system
 ratings_filtered = ratings_full[
     (ratings_full["movieId"].isin(surviving_movie_ids))
     & (ratings_full["userId"].isin(user_ids))
 ]
-ratings_filtered.to_csv(
-    os.path.join(script_directory, "preprocessed", "ratings.csv"), index=False
-)
+ratings_filtered.to_csv(make_output_path("ratings.csv"), index=False)
